0b92863560a6c6c4c03491577178f181d9d46eb7
bouthaina <bouthainabakouch@gmail.com>
2025-10-18 21:07:46 +0100

    update navbar

--- frontend-app/src/components/Navbar.jsx
+++ frontend-app/src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
 					bgGradient={"linear(to-r, cyan.400, blue.500)"}
 					bgClip={"text"}
 				>
-					<Link to={"/"}>Products - Store 🛒</Link>
+					<Link to={"/"}>All Products - Store 🛒</Link>
 				</Text>
 
 				<HStack spacing={2} alignItems={"center"}>
